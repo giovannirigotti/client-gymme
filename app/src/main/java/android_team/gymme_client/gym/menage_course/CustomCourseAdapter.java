3c1b12aec414782143ad10c3f27c415acde9a2b5
giovannirigotti <rigotti.giovanni@gmail.com>
2021-02-08 05:13:47 +0100

    finita listView corsi palestra. manca dialog visualizzazione e cancellazione

--- app/src/main/java/android_team/gymme_client/gym/menage_course/CustomCourseAdapter.java
+++ app/src/main/java/android_team/gymme_client/gym/menage_course/CustomCourseAdapter.java
@@ -1,4 +1,4 @@
-package android_team.gymme_client.gym.menage_worker;
+package android_team.gymme_client.gym.menage_course;
 
 import android.app.Activity;
 import android.app.Dialog;
@@ -31,85 +31,95 @@ import java.net.URL;
 import java.util.ArrayList;
 
 import android_team.gymme_client.R;
-import android_team.gymme_client.nutritionist.NutritionistObject;
+import android_team.gymme_client.gym.menage_worker.GymMenageWorkerActivity;
 import android_team.gymme_client.support.MyApplication;
+import android_team.gymme_client.trainer.TrainerObject;
 
-public class CustomGymNutritionistAssumedAdapter extends ArrayAdapter<NutritionistObject> {
+public class CustomCourseAdapter extends ArrayAdapter<CourseObject> {
 
-    private ArrayList<NutritionistObject> nutritionist;
+    private static ArrayList<CourseObject> courses;
     private Activity context;
 
-    public CustomGymNutritionistAssumedAdapter(Activity _context, ArrayList<NutritionistObject> _nutritionist) {
-        super(_context, R.layout.notification_item, _nutritionist);
+    public CustomCourseAdapter(Activity _context, ArrayList<CourseObject> _courses) {
+        super(_context, R.layout.notification_item, _courses);
         this.context = _context;
-        this.nutritionist = _nutritionist;
+        this.courses = _courses;
+    }
+
+    @Override
+    public int getCount() {
+        return courses.size();
     }
 
     @NonNull
     @Override
-    public View getView(final int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+    public View getView(final int position, @Nullable View convertView, @NonNull final ViewGroup parent) {
         View r = convertView;
-        CustomGymNutritionistAssumedAdapter.ViewHolder viewHolder = null;
+        CustomCourseAdapter.ViewHolder viewHolder = null;
         if (r == null) {
             LayoutInflater layoutInflater = context.getLayoutInflater();
-            r = layoutInflater.inflate(R.layout.gym_nutritionist_assumed_item, null);
-            viewHolder = new CustomGymNutritionistAssumedAdapter.ViewHolder(r);
+            r = layoutInflater.inflate(R.layout.gym_course_item, null);
+            viewHolder = new CustomCourseAdapter.ViewHolder(r);
             r.setTag(viewHolder);
         } else {
-            viewHolder = (CustomGymNutritionistAssumedAdapter.ViewHolder) r.getTag();
+            viewHolder = (CustomCourseAdapter.ViewHolder) r.getTag();
         }
 
-        final String nutritionist_id = nutritionist.get(position).user_id;
-        final String name = nutritionist.get(position).name;
-        final String lastname = nutritionist.get(position).lastname;
-        final String email = nutritionist.get(position).email;
-        final String qualification = nutritionist.get(position).qualification;
-        final String fiscal_code = nutritionist.get(position).fiscal_code;
+        final String name = courses.get(position).getTrainer_name();
+        final String lastname = courses.get(position).getTrainer_lastname();
+        final String description = courses.get(position).getDescription();
+        final String title = courses.get(position).getTitle();
+        final String category = courses.get(position).getCategory();
+        final String start_date = courses.get(position).getStart_date();
+        final String end_date = courses.get(position).getEnd_date();
+        final String max_persons = courses.get(position).getFree_spaces();
 
-        viewHolder.tv_gym_nutritionist_assumed_name.setText(name);
-        viewHolder.tv_gym_nutritionist_assumed_lastname.setText(lastname);
-        viewHolder.tv_gym_nutritionist_assumed_email.setText(email);
+        viewHolder.tv_course_item_title.setText(title);
+        viewHolder.tv_course_item_name.setText(name);
+        viewHolder.tv_course_item_lastname.setText(lastname);
 
-        viewHolder.btn_gym_nutritionist_assumed_add.setOnClickListener(new View.OnClickListener() {
+        viewHolder.btn_course_item_info.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                dismissNutritionist(context, nutritionist_id, name, lastname, email, qualification, fiscal_code, position);
+                //TODO dialog con informazioni
+                //dismissTrainer(context, trainer_id, name, lastname, email, qualification, fiscal_code, position);
             }
         });
         return r;
     }
 
     class ViewHolder {
-        TextView tv_gym_nutritionist_assumed_name,tv_gym_nutritionist_assumed_lastname,tv_gym_nutritionist_assumed_email;
-        ImageView btn_gym_nutritionist_assumed_add;
+        TextView tv_course_item_title, tv_course_item_name, tv_course_item_lastname;
+        ImageView btn_course_item_info;
 
         ViewHolder(View v) {
-            tv_gym_nutritionist_assumed_name = v.findViewById(R.id.tv_gym_nutritionist_assumed_name);
-            tv_gym_nutritionist_assumed_lastname = v.findViewById(R.id.tv_gym_nutritionist_assumed_lastname);
-            tv_gym_nutritionist_assumed_email = v.findViewById(R.id.tv_gym_nutritionist_assumed_email);
+            tv_course_item_title = v.findViewById(R.id.tv_course_item_title);
+            tv_course_item_name = v.findViewById(R.id.tv_course_item_name);
+            tv_course_item_lastname = v.findViewById(R.id.tv_course_item_lastname);
 
-            btn_gym_nutritionist_assumed_add = v.findViewById(R.id.btn_gym_nutritionist_assumed_add);
+            btn_course_item_info = v.findViewById(R.id.btn_course_item_info);
         }
     }
 
-    public void dismissNutritionist(Activity a, String nutritionist_id, String name, String lastname, String email, String qualification, String fiscal_code, Integer position) {
-        CustomGymNutritionistAssumedAdapter.CustomDialogRemoveNutritionist cdd = new CustomGymNutritionistAssumedAdapter.CustomDialogRemoveNutritionist(a, nutritionist_id, name, lastname, email, qualification, fiscal_code, position);
+/*
+    public void dismissTrainer(Activity a, String trainer_id, String name, String lastname, String email, String qualification, String fiscal_code, Integer position) {
+        CustomCourseAdapter.CustomDialogRemoveTrainer cdd = new CustomCourseAdapter.CustomDialogRemoveTrainer(a, trainer_id, name, lastname, email, qualification, fiscal_code, position);
         cdd.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         cdd.show();
     }
 
-    private class CustomDialogRemoveNutritionist extends Dialog implements View.OnClickListener {
+    private class CustomDialogRemoveTrainer extends Dialog implements View.OnClickListener {
 
         public Activity c;
         public Button Licenzia, Esci;
         public TextView _name, _lastname, _email, _qualification, _fiscal_code;
-        public String nutritionist_id, name, lastname, email, qualification, fiscal_code;
+        public String trainer_id, name, lastname, email, qualification, fiscal_code;
         Integer position;
 
-        public CustomDialogRemoveNutritionist(Activity a, String nutritionist_id, String name, String lastname, String email, String qualification, String fiscal_code, Integer position) {
+        public CustomDialogRemoveTrainer(Activity a, String trainer_id, String name, String lastname, String email, String qualification, String fiscal_code, Integer position) {
             super(a);
             this.c = a;
-            this.nutritionist_id = nutritionist_id;
+            this.trainer_id = trainer_id;
             this.name = name;
             this.lastname = lastname;
             this.email = email;
@@ -123,7 +133,7 @@ public class CustomGymNutritionistAssumedAdapter extends ArrayAdapter<Nutritioni
 
             super.onCreate(savedInstanceState);
             requestWindowFeature(Window.FEATURE_NO_TITLE);
-            setContentView(R.layout.dialog_dismiss_nutritionist);
+            setContentView(R.layout.dialog_dismiss_trainer);
             Licenzia = (Button) findViewById(R.id.dialog_confirm_user_type_yes);
             Esci = (Button) findViewById(R.id.dialog_confirm_user_type_no);
 
@@ -149,14 +159,14 @@ public class CustomGymNutritionistAssumedAdapter extends ArrayAdapter<Nutritioni
 
             switch (v.getId()) {
                 case R.id.dialog_confirm_user_type_yes:
-                    CustomGymNutritionistAssumedAdapter.DismissNutritionistConnection asyncTask = (CustomGymNutritionistAssumedAdapter.DismissNutritionistConnection) new CustomGymNutritionistAssumedAdapter.DismissNutritionistConnection(new CustomGymNutritionistAssumedAdapter.DismissNutritionistConnection.AsyncResponse() {
+                    CustomCourseAdapter.DismissTrainerConnection asyncTask = (CustomCourseAdapter.DismissTrainerConnection) new CustomCourseAdapter.DismissTrainerConnection(new CustomCourseAdapter.DismissTrainerConnection.AsyncResponse() {
                         @Override
                         public void processFinish(Integer output) {
                             if (output == 200) {
                                 GymMenageWorkerActivity.runOnUI(new Runnable() {
                                     public void run() {
-                                        Toast.makeText(MyApplication.getContext(), "SUCCESS, nutritionist licenziato", Toast.LENGTH_SHORT).show();
-                                        GymMenageWorkerActivity.redoAdapterNutri(context, nutritionist, position);
+                                        Toast.makeText(MyApplication.getContext(), "SUCCESS, trainer licenziato", Toast.LENGTH_SHORT).show();
+                                        GymMenageWorkerActivity.redoAdapterTrainer(context, trainers, position);
                                     }
                                 });
                             } else {
@@ -167,7 +177,7 @@ public class CustomGymNutritionistAssumedAdapter extends ArrayAdapter<Nutritioni
                                 });
                             }
                         }
-                    }).execute(nutritionist_id, GymMenageWorkerActivity.getGymId());
+                    }).execute(trainer_id, GymMenageWorkerActivity.getGymId());
                     dismiss();
                     break;
                 case R.id.dialog_confirm_user_type_no:
@@ -181,16 +191,16 @@ public class CustomGymNutritionistAssumedAdapter extends ArrayAdapter<Nutritioni
 
     }
 
-    public static class DismissNutritionistConnection extends AsyncTask<String, String, Integer> {
+    public static class DismissTrainerConnection extends AsyncTask<String, String, Integer> {
 
         // you may separate this or combined to caller class.
         public interface AsyncResponse {
             void processFinish(Integer output);
         }
 
-        public CustomGymNutritionistAssumedAdapter.DismissNutritionistConnection.AsyncResponse delegate = null;
+        public CustomCourseAdapter.DismissTrainerConnection.AsyncResponse delegate = null;
 
-        public DismissNutritionistConnection(CustomGymNutritionistAssumedAdapter.DismissNutritionistConnection.AsyncResponse delegate) {
+        public DismissTrainerConnection(CustomCourseAdapter.DismissTrainerConnection.AsyncResponse delegate) {
             this.delegate = delegate;
         }
 
@@ -201,7 +211,7 @@ public class CustomGymNutritionistAssumedAdapter extends ArrayAdapter<Nutritioni
             JsonObject user = null;
             int responseCode = 500;
             try {
-                url = new URL("http://10.0.2.2:4000/gym/dismiss_nutritionist/");
+                url = new URL("http://10.0.2.2:4000/gym/dismiss_trainer/");
                 urlConnection = (HttpURLConnection) url.openConnection();
                 urlConnection.setRequestMethod("POST");
                 urlConnection.setConnectTimeout(5000);
@@ -247,4 +257,7 @@ public class CustomGymNutritionistAssumedAdapter extends ArrayAdapter<Nutritioni
         }
 
     }
+
+    */
+
 }
--- app/src/main/java/android_team/gymme_client/gym/menage_course/GymCourseActivity.java
+++ app/src/main/java/android_team/gymme_client/gym/menage_course/GymCourseActivity.java
@@ -2,7 +2,9 @@ package android_team.gymme_client.gym.menage_course;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -10,16 +12,31 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 import android_team.gymme_client.R;
+import android_team.gymme_client.gym.menage_worker.CustomGymTrainerAssumedAdapter;
+import android_team.gymme_client.gym.menage_worker.GymMenageWorkerActivity;
 import android_team.gymme_client.login.LoginActivity;
+import android_team.gymme_client.trainer.TrainerObject;
 
 public class GymCourseActivity extends AppCompatActivity {
 
     private int user_id;
 
     static ArrayList<CourseObject> courses_list;
+    static CustomCourseAdapter course_adapter;
 
     static ListView lv_my_courses;
     private Button btn_create_course;
@@ -46,8 +63,13 @@ public class GymCourseActivity extends AppCompatActivity {
         }
         //endregion
 
+        //inizializzo
         lv_my_courses = (ListView) findViewById(R.id.lv_my_courses);
         btn_create_course = (Button) findViewById(R.id.btn_create_course);
+        
+        //carico corsi
+        loadCourses();
+        
 
         btn_create_course.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,6 +81,130 @@ public class GymCourseActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void loadCourses() {
+        GymCourseActivity.ReceiveCourseConn asyncTaskUser = (GymCourseActivity.ReceiveCourseConn) new GymCourseActivity.ReceiveCourseConn(new GymCourseActivity.ReceiveCourseConn.AsyncResponse() {
+            @Override
+            public void processFinish(ArrayList<CourseObject> courses) {
+
+                courses_list = courses;
+                if (courses_list.size() > 0) {
+                    //DATI RICEVUTI
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            //setto tramite l'adapter la lista dei trainer da visualizzare nella recycler view(notificationView)
+                            course_adapter = new CustomCourseAdapter(GymCourseActivity.this, courses_list);
+                            lv_my_courses.setAdapter(course_adapter);
+
+                        }
+                    });
+                } else {
+                    // NESSUN DATO RICEVUTO PERCHE' NESSUNA TRAINER LAVORA PER QUESTA PALESTRA
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            Toast.makeText(GymCourseActivity.this, "Nessun corso", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+
+        }).execute(String.valueOf(user_id));
+    }
+
+    private static class ReceiveCourseConn extends AsyncTask<String, String, JsonArray> {
+
+        public interface AsyncResponse {
+            void processFinish(ArrayList<CourseObject> courses);
+        }
+
+        public GymCourseActivity.ReceiveCourseConn.AsyncResponse delegate = null;
+
+        public ReceiveCourseConn(GymCourseActivity.ReceiveCourseConn.AsyncResponse delegate) {
+            this.delegate = delegate;
+        }
+
+        @SuppressLint("WrongThread")
+        @Override
+        protected JsonArray doInBackground(String... params) {
+
+            URL url;
+            HttpURLConnection urlConnection = null;
+            JsonArray _courses = null;
+            ArrayList<CourseObject> c_objects = new ArrayList<>();
+
+            try {
+                url = new URL("http://10.0.2.2:4000/gym/get_courses/" + params[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.setConnectTimeout(5000);
+                urlConnection.connect();
+                int responseCode = urlConnection.getResponseCode();
+                urlConnection.disconnect();
+
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+
+                    Log.e("Server response", "HTTP_OK");
+                    String responseString = readStream(urlConnection.getInputStream());
+                    _courses = JsonParser.parseString(responseString).getAsJsonArray();
+
+                    for (int i = 0; i < _courses.size(); i++) {
+                        JsonObject course = (JsonObject) _courses.get(i);
+
+                        String name = course.get("name").getAsString().trim();
+                        String lastname = course.get("lastname").getAsString().trim();
+                        String description = course.get("description").getAsString().trim();
+                        String title = course.get("title").getAsString().trim();
+                        String category = course.get("category").getAsString().trim();
+                        String start_date = course.get("start_date").getAsString().trim();
+                        String end_date = course.get("end_date").getAsString().trim();
+                        String max_persons = course.get("max_persons").getAsString().trim();
+
+                        CourseObject c_obj = new CourseObject(name, lastname, description, title, category, start_date, end_date, max_persons);
+                        c_objects.add(c_obj);
+
+                    }
+                    //SE VA TUTTO A BUON FINE INVIO AL METODO procesFinish();
+                    delegate.processFinish(c_objects);
+
+                } else if (responseCode == HttpURLConnection.HTTP_NOT_FOUND) {
+                    Log.e("GET TRAINER", "response: HTTP_NOT_FOUND");
+                    delegate.processFinish(new ArrayList<CourseObject>());
+                } else {
+                    Log.e("GET TRAINER", "SERVER ERROR");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.e("GET TRAINER", "I/O EXCEPTION ERROR");
+            } finally {
+                if (urlConnection != null)
+                    urlConnection.disconnect();
+            }
+            return _courses;
+        }
+
+        private String readStream(InputStream in) throws UnsupportedEncodingException {
+            BufferedReader reader = null;
+            StringBuffer response = new StringBuffer();
+            try {
+                reader = new BufferedReader(new InputStreamReader(in));
+                String line = "";
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return response.toString();
+        }
     }
 
 
