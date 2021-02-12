9fbcbcf687659327a358ef4bbdc77f719a927f0d
giovannirigotti <rigotti.giovanni@gmail.com>
2021-02-12 19:02:15 +0100

    Aggiunta pagina (da testare) per visualizzare lista trainer_customer

---
+++ app/src/main/java/android_team/gymme_client/trainer/menage_trainig_sheet/CustomTrainerCustomerAdapter.java
@@ -0,0 +1,297 @@
+package android_team.gymme_client.trainer.menage_trainig_sheet;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.gson.JsonObject;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+import android_team.gymme_client.R;
+import android_team.gymme_client.customer.CustomerSmallObject;
+import android_team.gymme_client.gym.menage_customer.GymCustomersActivity;
+import android_team.gymme_client.gym.menage_worker.GymMenageWorkerActivity;
+import android_team.gymme_client.support.MyApplication;
+
+public class CustomTrainerCustomerAdapter extends ArrayAdapter<CustomerSmallObject> implements Filterable {
+
+    private static ArrayList<CustomerSmallObject> customers;
+    private Activity context;
+
+    public CustomTrainerCustomerAdapter(Activity _context, ArrayList<CustomerSmallObject> customers) {
+        super(_context, R.layout.notification_item, customers);
+        this.context = _context;
+        this.customers = customers;
+    }
+
+    @Override
+    public int getCount() {
+        return customers.size();
+    }
+
+    @NonNull
+    @Override
+    public View getView(final int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View r = convertView;
+        CustomTrainerCustomerAdapter.ViewHolder viewHolder = null;
+        if (r == null) {
+            LayoutInflater layoutInflater = context.getLayoutInflater();
+            r = layoutInflater.inflate(R.layout.gym_trainer_assumed_item, null);
+            viewHolder = new CustomTrainerCustomerAdapter.ViewHolder(r);
+            r.setTag(viewHolder);
+        } else {
+            viewHolder = (CustomTrainerCustomerAdapter.ViewHolder) r.getTag();
+        }
+
+        final String user_id = customers.get(position).getUser_id();
+        final String name = customers.get(position).getName();
+        final String lastname = customers.get(position).getLasname();
+        final String email = customers.get(position).getEmail();
+        final String birthdate = customers.get(position).getBirthdate();
+
+        viewHolder.tv_gym_trainer_name.setText(name);
+        viewHolder.tv_gym_trainer_lastname.setText(lastname);
+        viewHolder.tv_gym_trainer_email.setText(email);
+
+        viewHolder.btn_gym_trainer_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO: Visualizzo schede utente selezionato
+            }
+        });
+        return r;
+    }
+
+    class ViewHolder {
+        TextView tv_gym_trainer_name, tv_gym_trainer_lastname, tv_gym_trainer_email;
+        ImageView btn_gym_trainer_add;
+
+        ViewHolder(View v) {
+            tv_gym_trainer_name = v.findViewById(R.id.tv_gym_trainer_assumed_name);
+            tv_gym_trainer_lastname = v.findViewById(R.id.tv_gym_trainer_assumed_lastname);
+            tv_gym_trainer_email = v.findViewById(R.id.tv_gym_trainer_assumed_email);
+            btn_gym_trainer_add = v.findViewById(R.id.btn_gym_trainer_assumed_add);
+        }
+    }
+
+    @NonNull
+    @Override
+    public Filter getFilter() {
+        Filter filter = new Filter() {
+
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                FilterResults results = new FilterResults();
+                ArrayList<CustomerSmallObject> allCustomers = GymCustomersActivity.getAllCustomers();
+                if (constraint == null || constraint.length() == 0) {
+                    results.values = allCustomers;
+                    results.count = allCustomers.size();
+                } else {
+                    ArrayList<CustomerSmallObject> FilteredCustomers = new ArrayList<CustomerSmallObject>();
+                    // perform your search here using the searchConstraint String.
+                    constraint = constraint.toString().toLowerCase();
+                    for (CustomerSmallObject c : customers) {
+                        String dataNames = c.getName();
+                        if (dataNames.toLowerCase().startsWith(constraint.toString())) {
+                            FilteredCustomers.add(c);
+                        }
+                    }
+                    results.values = FilteredCustomers;
+                    results.count = FilteredCustomers.size();
+                    Log.e("VALUES", results.values.toString());
+                }
+
+                return results;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                Log.e("TEST", results.values.toString());
+                customers = (ArrayList<CustomerSmallObject>) results.values;
+                notifyDataSetChanged();
+            }
+
+
+        };
+        return filter;
+    }
+
+    public void InfoCustomer(Activity a, String user_id, String name, String lastname, String email, String birthdate, Integer position) {
+        CustomTrainerCustomerAdapter.CustomDialogCustomerInfo cdd = new CustomTrainerCustomerAdapter.CustomDialogCustomerInfo(a, user_id, name, lastname, email, birthdate, position);
+        cdd.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        cdd.show();
+    }
+
+    private class CustomDialogCustomerInfo extends Dialog implements View.OnClickListener {
+
+        public Activity c;
+        public Button Rimouvi, Esci;
+        public TextView _name, _lastname, _email, _birthdate;
+        public String user_id, name, lastname, email, birthdate;
+        Integer position;
+
+        public CustomDialogCustomerInfo(Activity a, String user_id, String name, String lastname, String email, String birthdate, Integer position) {
+            super(a);
+            this.c = a;
+            this.user_id = user_id;
+            this.name = name;
+            this.lastname = lastname;
+            this.email = email;
+            this.birthdate= birthdate;
+            this.position = position;
+        }
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+
+            super.onCreate(savedInstanceState);
+            requestWindowFeature(Window.FEATURE_NO_TITLE);
+            setContentView(R.layout.dialog_info_costumer);
+            Rimouvi = (Button) findViewById(R.id.dialog_remove_customer);
+            Esci = (Button) findViewById(R.id.dialog_exit_from_info_cutomer);
+
+            _name = (TextView) findViewById(R.id.tv_dismiss_name);
+            _lastname = (TextView) findViewById(R.id.tv_dismiss_lastname);
+            _email = (TextView) findViewById(R.id.tv_dismiss_email);
+            _birthdate = (TextView) findViewById(R.id.tv_dismiss_birthdate);
+
+            _name.setText(name);
+            _lastname.setText(lastname);
+            _email.setText(email);
+            _birthdate.setText(birthdate.split("T")[0]);
+
+            Rimouvi.setOnClickListener(this);
+            Esci.setOnClickListener(this);
+        }
+
+
+        @Override
+        public void onClick(View v) {
+
+            switch (v.getId()) {
+                case R.id.dialog_remove_customer:
+                    CustomTrainerCustomerAdapter.RemoveCustomerConncection asyncTask = (CustomTrainerCustomerAdapter.RemoveCustomerConncection) new CustomTrainerCustomerAdapter.RemoveCustomerConncection(new CustomTrainerCustomerAdapter.RemoveCustomerConncection.AsyncResponse() {
+                        @Override
+                        public void processFinish(Integer output) {
+                            if (output == 200) {
+                                GymMenageWorkerActivity.runOnUI(new Runnable() {
+                                    public void run() {
+                                        Toast.makeText(MyApplication.getContext(), "SUCCESS, Cliente rimosso", Toast.LENGTH_SHORT).show();
+                                        GymCustomersActivity.redirectManage(context);
+                                    }
+                                });
+                            } else {
+                                GymMenageWorkerActivity.runOnUI(new Runnable() {
+                                    public void run() {
+                                        Toast.makeText(MyApplication.getContext(), "ERRORE, server side", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                            }
+                        }
+                    }).execute(user_id, GymCustomersActivity.getGymId());
+                    dismiss();
+                    break;
+                case R.id.dialog_exit_from_info_cutomer:
+                    //
+                    dismiss();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+
+    public static class RemoveCustomerConncection extends AsyncTask<String, String, Integer> {
+
+        // you may separate this or combined to caller class.
+        public interface AsyncResponse {
+            void processFinish(Integer output);
+        }
+
+        public CustomTrainerCustomerAdapter.RemoveCustomerConncection.AsyncResponse delegate = null;
+
+        public RemoveCustomerConncection(CustomTrainerCustomerAdapter.RemoveCustomerConncection.AsyncResponse delegate) {
+            this.delegate = delegate;
+        }
+
+        @Override
+        protected Integer doInBackground(String... params) {
+            URL url;
+            HttpURLConnection urlConnection = null;
+            JsonObject user = null;
+            int responseCode = 500;
+            try {
+                url = new URL("http://10.0.2.2:4000/gym/delete_gym_customer/");
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setConnectTimeout(5000);
+                urlConnection.setRequestProperty("Content-Type", "application/json");
+
+                JsonObject paramsJson = new JsonObject();
+
+                paramsJson.addProperty("user_id", params[0]);
+                paramsJson.addProperty("gym_id", params[1]);
+
+                urlConnection.setDoOutput(true);
+
+                OutputStream os = urlConnection.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+                writer.write(paramsJson.toString());
+                writer.flush();
+                writer.close();
+                os.close();
+
+                urlConnection.connect();
+                responseCode = urlConnection.getResponseCode();
+
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    Log.e("GYM CUSTOMER", "Cancellazione ok");
+                    responseCode = 200;
+                    delegate.processFinish(responseCode);
+                } else {
+                    Log.e("GYM CUSTOMER", "Error cancellazione");
+                    responseCode = 500;
+                    delegate.processFinish(responseCode);
+                    urlConnection.disconnect();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                responseCode = 69;
+                Log.e("GYM CUSTOMER", "Error I/O");
+                delegate.processFinish(responseCode);
+            } finally {
+                if (urlConnection != null)
+                    urlConnection.disconnect();
+            }
+            return responseCode;
+        }
+
+    }
+}
--- app/src/main/java/android_team/gymme_client/trainer/menage_trainig_sheet/TrainerMenageTrainingSheet.java
+++ app/src/main/java/android_team/gymme_client/trainer/menage_trainig_sheet/TrainerMenageTrainingSheet.java
@@ -3,18 +3,40 @@ package android_team.gymme_client.trainer.menage_trainig_sheet;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
 import android_team.gymme_client.R;
+import android_team.gymme_client.customer.CustomerSmallObject;
 import android_team.gymme_client.gym.DrawerGymListener;
+import android_team.gymme_client.gym.menage_customer.CustomGymCustomerAdapter;
+import android_team.gymme_client.gym.menage_customer.GymCustomersActivity;
+import android_team.gymme_client.gym.menage_worker.CustomGymTrainerAdapter;
 import android_team.gymme_client.login.LoginActivity;
 import android_team.gymme_client.support.Drawer;
 import android_team.gymme_client.trainer.DrawerTrainerListener;
+import android_team.gymme_client.trainer.TrainerObject;
 
 public class TrainerMenageTrainingSheet extends AppCompatActivity {
 
@@ -23,6 +45,11 @@ public class TrainerMenageTrainingSheet extends AppCompatActivity {
     DrawerTrainerListener drawerTrainerListener;
     DrawerLayout drawerLayout;
     TextView tv_title;
+
+    public static ArrayList<CustomerSmallObject> customer_list;
+    static CustomTrainerCustomerAdapter customer_adapter;
+    static ListView lv_customer;
+    EditText inputSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,10 +71,135 @@ public class TrainerMenageTrainingSheet extends AppCompatActivity {
             }
         }
 
+        lv_customer = (ListView) findViewById(R.id.lv_trainer_customer);
+        inputSearch = (EditText) findViewById(R.id.et_search_trainer_customer);
+
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout_trainer_activity);
         drawerTrainerListener = new DrawerTrainerListener(this, user_id);
         tv_title = (TextView) findViewById(R.id.main_toolbar_title);
         tv_title.setText("SCHEDE ALLENAMENTI");
+
+        getTrainerCustomer();
+    }
+
+    private void getTrainerCustomer() {
+        TrainerMenageTrainingSheet.ReceiveCustomerConnection asyncTaskUser = (TrainerMenageTrainingSheet.ReceiveCustomerConnection) new TrainerMenageTrainingSheet.ReceiveCustomerConnection(new TrainerMenageTrainingSheet.ReceiveCustomerConnection.AsyncResponse() {
+            @Override
+            public void processFinish(ArrayList<CustomerSmallObject> customers) {
+                customer_list = customers;
+                if (customer_list.size() > 0) {
+                    //DATI RICEVUTI
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            //setto tramite l'adapter la lista dei trainer da visualizzare nella recycler view(notificationView)
+                            customer_adapter = new CustomTrainerCustomerAdapter(TrainerMenageTrainingSheet.this, customer_list);
+                            lv_customer.setAdapter(customer_adapter);
+
+                        }
+                    });
+                } else {
+                    // NESSUN DATO RICEVUTO PERCHE' NESSUNA TRAINER LAVORA PER QUESTA PALESTRA
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            Toast.makeText(TrainerMenageTrainingSheet.this, "Nessun cliente nella tua palestra", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+
+        }).execute(String.valueOf(user_id));
+    }
+
+    private static class ReceiveCustomerConnection extends AsyncTask<String, String, JsonArray> {
+
+        public interface AsyncResponse {
+            void processFinish(ArrayList<CustomerSmallObject> customers);
+        }
+
+        public TrainerMenageTrainingSheet.ReceiveCustomerConnection.AsyncResponse delegate = null;
+
+        public ReceiveCustomerConnection(TrainerMenageTrainingSheet.ReceiveCustomerConnection.AsyncResponse delegate) {
+            this.delegate = delegate;
+        }
+
+        @SuppressLint("WrongThread")
+        @Override
+        protected JsonArray doInBackground(String... params) {
+
+            URL url;
+            HttpURLConnection urlConnection = null;
+            JsonArray _customers = null;
+            ArrayList<CustomerSmallObject> t_objects = new ArrayList<CustomerSmallObject>();
+
+            try {
+                url = new URL("http://10.0.2.2:4000/trainer/get_customers/" + params[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.setConnectTimeout(5000);
+                urlConnection.connect();
+                int responseCode = urlConnection.getResponseCode();
+                urlConnection.disconnect();
+
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+
+                    Log.e("Server response", "HTTP_OK");
+                    String responseString = readStream(urlConnection.getInputStream());
+                    _customers = JsonParser.parseString(responseString).getAsJsonArray();
+
+                    for (int i = 0; i < _customers.size(); i++) {
+                        JsonObject customer = (JsonObject) _customers.get(i);
+
+                        String user_id = customer.get("user_id").getAsString().trim();
+                        String name = customer.get("name").getAsString().trim();
+                        String lastname = customer.get("lastname").getAsString().trim();
+                        String email = customer.get("email").getAsString().trim();
+                        String birthdate = customer.get("birthdate").getAsString().trim();
+
+                        CustomerSmallObject t_obj = new CustomerSmallObject(user_id, name, lastname, email, birthdate);
+                        t_objects.add(t_obj);
+
+                    }
+                    //SE VA TUTTO A BUON FINE INVIO AL METODO procesFinish();
+                    delegate.processFinish(t_objects);
+
+                } else if (responseCode == HttpURLConnection.HTTP_NOT_FOUND) {
+                    Log.e("GET CUSTOMERS", "response: HTTP_NOT_FOUND");
+                    delegate.processFinish(new ArrayList<CustomerSmallObject>());
+                } else {
+                    Log.e("GET CUSTOMERS", "SERVER ERROR");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.e("GET CUSTOMERS", "I/O EXCEPTION ERROR");
+            } finally {
+                if (urlConnection != null)
+                    urlConnection.disconnect();
+            }
+            return _customers;
+        }
+
+        private String readStream(InputStream in) throws UnsupportedEncodingException {
+            BufferedReader reader = null;
+            StringBuffer response = new StringBuffer();
+            try {
+                reader = new BufferedReader(new InputStreamReader(in));
+                String line = "";
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return response.toString();
+        }
     }
 
     //region DRAWER
