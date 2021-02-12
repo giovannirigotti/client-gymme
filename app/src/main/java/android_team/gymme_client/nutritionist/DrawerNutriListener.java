6b35f2f8fb2136667c533f610610b4c5fea9c75f
giovannirigotti <rigotti.giovanni@gmail.com>
2021-02-12 03:56:13 +0100

    Aggiunto e finito drawer nutrizionista

---
+++ app/src/main/java/android_team/gymme_client/nutritionist/DrawerNutriListener.java
@@ -0,0 +1,35 @@
+package android_team.gymme_client.nutritionist;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
+
+import android_team.gymme_client.trainer.TrainerHomeActivity;
+import android_team.gymme_client.trainer.TrainerProfileActivity;
+import android_team.gymme_client.trainer.menage_trainig_sheet.TrainerMenageTrainingSheet;
+
+public class DrawerNutriListener {
+
+    Activity activity;
+    Integer user_id;
+
+    public DrawerNutriListener(Activity activity, Integer user_id) {
+        this.activity = activity;
+        this.user_id = user_id;
+    }
+
+    public void toProfile() {
+        redirectActivity(activity, NutritionistProfileActivity.class);
+    }
+    public void toHome() {
+        redirectActivity(activity, NutritionistHomeActivity.class);
+    }
+
+    public void redirectActivity(Activity a, Class c) {
+        Log.e("REDIRECT", c.toString());
+        Intent i = new Intent(a, c);
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        i.putExtra("user_id", user_id);
+        a.startActivity(i);
+    }
+}
